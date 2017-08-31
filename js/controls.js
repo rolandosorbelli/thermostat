bd3657765bddd5ff46642503c239a04c88381648
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-08-31 16:10:21 +0100

    added temperture up and down interface

--- js/controls.js
+++ js/controls.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 	var thermostat = new Thermostat();
 	$('#temperature').text(thermostat.temperature);
+
+	$('#temperature-up').on('click', function() { // event listener
+		thermostat.up(); // update model
+		$('#temperature').text(thermostat.temperature); // update view
+	})
+
+	$('#temperature-down').on('click', function() {
+		thermostat.down();
+		$('#temperature').text(thermostat.temperature);
+	})
+
+
 })
